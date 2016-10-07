de84ab478e0d0bbf26301273d235ae8dc6dab97a
leakvoid <leakvoid@gmail.com>
2016-10-07 20:10:56 +0300

    requested user stories has been implemented

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 /* initialization */
 var express = require('express');
+var multer = require('multer');
+
+var upload = multer({ dest: 'uploads/' });
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 /* routing */
 app.get('/', function(req, res) {
-    res.send('file metadata test');
+    res.sendFile(__dirname + '/static_pages/home.html');
+});
+
+app.post('/upload_file', upload.single('uploaded_file'), function(req, res) {
+    console.log(JSON.stringify(req.file));
+    res.send({'file_size': req.file.size});
 });
 
 /* misc */
+app.get('/favicon.ico', function(req, res) {
+    console.log('piss off, favicon!');
+    res.end('piss off, favicon!');
+});
+
+app.use(function(req, res) {
+    console.log('Requested page not found: 404');
+    res.end('Requested page not found: 404');
+});
+
 app.listen(port, function() {
     console.log('Server running at port ' + port);
 });
